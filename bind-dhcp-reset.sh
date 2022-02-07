3dbc64d894e6c667922eb07c1de89b4314be5d13
egberts <>
2022-02-07 14:07:24 -0500

    Add more field checking. 'rndc refresh' needs a zone parameter. reorder sequence of 'rndc' commands before shutdown.

--- bind-dhcp-reset.sh
+++ bind-dhcp-reset.sh
@@ -14,69 +14,141 @@
 # ISC Bind9 v9.15
 # ISC DHCP v4.4.1
 
-PUBLIC_DOMAIN_FQDN="example.com"
-PUBLIC_BIND_DIR="/etc/bind/public"
-PUBLIC_NAMED_CONF="/etc/bind/named-public.conf"
-PUBLIC_RNDC_CONF="/etc/bind/rndc-public.conf"
+# Debian Stretch/Woody/Sid/Bullseye filesystem convention
+NAMED_ETC_DIR=/etc/bind
+
+PUBLIC_DOMAIN_FQDN="egbert.net"
+# PUBLIC_BIND_DIR="${NAMED_ETC_DIR}/public"
+PUBLIC_NAMED_CONF="${NAMED_ETC_DIR}/named-public.conf"
+PUBLIC_RNDC_CONF="${NAMED_ETC_DIR}/rndc-public.conf"
 PUBLIC_RNDC_OPT="-p 954"
-INTERNAL_DOMAIN_FQDN="local"
-INTERNAL_BIND_DIR="/etc/bind/internal"
-INTERNAL_NAMED_CONF="/etc/bind/named-internal.conf"
-INTERNAL_RNDC_CONF="/etc/bind/rndc.conf"
+INTERNAL_DOMAIN_FQDN="leo"
+# INTERNAL_BIND_DIR="${NAMED_ETC_DIR}/internal"
+INTERNAL_NAMED_CONF="${NAMED_ETC_DIR}/named-internal.conf"
+INTERNAL_RNDC_CONF="${NAMED_ETC_DIR}/rndc.conf"
 INTERNAL_RNDC_OPT="-p 953"
 INTERNAL_VIEWED_DYNAMIC_ZONE="green"
 
-# check_zone <zone-name> <class> <zone-db-file>
-function check_zone()
-{
-  echo "Performing named-checkzone -c $2 $1 $3..."
-  named-checkzone \
-        -i full \
-        -S warn \
-        -c $2 \
-        $1 $3
+function check_systemctl_status() {
+  SYSTEMD_UNIT_NAME=$1
+  # Check status of DHCP server, Bind-Internal, then Bind-External
+  systemctl status -l --lines=20 --no-pager "$SYSTEMD_UNIT_NAME"
   RET_STS=$?
   if [[ ${RET_STS} -ne 0 ]]; then
-    echo "ERROR: Zone: $1 File: $3: Errno: $RET_STS"
+    echo "Ouch; Starting up DHCP server failed"
+    exit $RET_STS
+  fi
+}
+
+function rndc_zonestatus_internal() {
+  echo "Zone Status: Internal"
+  rndc "$INTERNAL_RNDC_OPT" \
+       -c "$INTERNAL_RNDC_CONF" \
+            zonestatus "$INTERNAL_DOMAIN_FQDN"
+}
+
+function rndc_zonestatus_public() {
+  echo "Zone Status: Public"
+  rndc "$PUBLIC_RNDC_OPT" \
+       -c "$PUBLIC_RNDC_CONF" \
+       zonestatus "$PUBLIC_DOMAIN_FQDN"
+}
+
+function bdr_bind_files_remove() {
+  # most of the removes here are for single Bind9 instantiation.
+  rm -f /var/cache/bind/*.jbk
+  rm -f /var/lib/bind/*.jnl
+  rm -f /var/lib/bind/*.signed
+}
+
+function bdr_bind_files_remove_internal() {
+  rm -f /var/cache/bind/internal/*.jbk
+  # rm -f /var/lib/bind/internal/*.jnl
+  # rm -f /var/lib/bind/internal/*.signed
+  rm -f /var/lib/bind/internal/master/*.jnl
+  rm -f /var/lib/bind/internal/master/*.signed
+  rm -f /var/lib/bind/internal/slave/*.jnl
+  rm -f /var/lib/bind/internal/slave/*.signed
+  rm -f /var/lib/bind/internal/dynamic/*.jnl
+  rm -f /var/lib/bind/internal/dynamic/*.signed
+}
+
+function bdr_bind_files_remove_public() {
+  rm -f /var/cache/bind/public/*.jbk
+  # rm -f /var/lib/bind/public/*.jnl
+  # rm -f /var/lib/bind/public/*.signed
+  rm -f /var/lib/bind/public/master/*.jnl
+  rm -f /var/lib/bind/public/master/*.signed
+  rm -f /var/lib/bind/public/slave/*.jnl
+  rm -f /var/lib/bind/public/slave/*.signed
+  rm -f /var/lib/bind/public/dynamic/*.jnl
+  rm -f /var/lib/bind/public/dynamic/*.signed
+}
+
+function zone_slaves_retransfer() {
+  # Now regather the zone statuses and note their master/slave ones
+  zone_slaves_list internal
+  # ZONES_SLAVE is returned
+  for THIS_ZONE in $ZONES_SLAVE; do
+    # Inform downstream (slaves) nameservers of  new  zones
+    echo "Retransfering: Internal $THIS_ZONE slave zone"
+    # Tell upstream master nameserver to do AXFR again
+    rndc "$INTERNAL_RNDC_OPT" \
+      -c "$INTERNAL_RNDC_CONF" \
+      retransfer "$PUBLIC_DOMAIN_FQDN"
+  done
+}
+
+# check_zone <zone-name> <class> <zone-db-file>
+function check_zone_data() {
+  NCZ_ARG_ZONENAME=$1
+  NCZ_ARG_CONFIG=$2
+  NCZ_ARG_ZONEFILE=$3
+  echo "Performing named-checkzone -c $NCZ_ARG_CONFIG $NCZ_ARG_ZONENAME $NCZ_ARG_ZONEFILE..."
+  named-checkzone \
+    -i full \
+    -S warn \
+    -c "${NCZ_ARG_CONFIG}" \
+    "${NCZ_ARG_ZONENAME}" "${NCZ_ARG_ZONEFILE}"
+  RET_STS=$?
+  if [[ ${RET_STS} -ne 0 ]]; then
+    echo "ERROR: Zone: $NCZ_ARG_ZONENAME File: $NCZ_ARG_CONFIG: Errno: $RET_STS"
     exit ${RET_STS}
   fi
 }
 
-
 # must_have_include <named.conf-path>
-function must_have_include()
-{
+function must_have_include() {
+  MHI_INCLUDE_FILE=$1
   # echo "Checking that named.conf is broken into many files via include statements..."
-  INCLUDE_STATEMENT_COUNT="`cat $1 | egrep -c '^[[:space:]]*include '`"
+  INCLUDE_STATEMENT_COUNT="$(cat <"${MHI_INCLUDE_FILE}" | grep -E -c '^[[:space:]]*include ')"
   if [[ -z ${INCLUDE_STATEMENT_COUNT} ]]; then
     echo "No include statement found in $1"
-    exit -1
+    exit 2 # ENOENT
   fi
 }
 
-
 # add_any_include_files <filespec>
-function add_any_include_files()
-{
+function add_any_include_files() {
+  AAIF_ARG_CONF_FILE=$1
   # echo "Include statements: $INCLUDE_STATEMENT_COUNT"
   # Make a list of include statements to read
-  INCLUDE_STATEMENTS="`cat $1 | egrep '^[[:space:]]*include ' | awk -F '"' '{print $2}'`"
+  INCLUDE_STATEMENTS="$(cat <"${AAIF_ARG_CONF_FILE}" | grep -E '^[[:space:]]*include ' | awk -F '"' '{print $2}')"
   # echo "add_any_include_files: $INCLUDE_STATEMENTS"
 }
 
 # reduce_include_files <list>
-function reduce_include_files
-{
+function reduce_include_files() {
   RET_INC_FILES=
   INC_FILES=$1
   for THIS_INC in ${INC_FILES}; do
     # echo "Checking if $THISINC has any zone/view statements..."
     # find any zone or view
-    ZONES_FOUND="`cat ${THIS_INC} | egrep -c '^[[:space:]]*zone '`"
-    ZONE_STATEMENT="`cat ${THIS_INC} | egrep '^[[:space:]]*zone '`"
-    ZONENAME="`echo ${ZONE_STATEMENT} | awk -F ' ' '{print $2}' | sed -e 's/\"//g'`"
+    ZONES_FOUND="$(cat <"${THIS_INC}" | grep -E -c '^[[:space:]]*zone ')"
+    ZONE_STATEMENT="$(cat <"${THIS_INC}" | grep -E '^[[:space:]]*zone ')"
+    ZONENAME="$(echo "${ZONE_STATEMENT}" | awk -F ' ' '{print $2}' | sed -e 's/\"//g')"
     if [[ ${ZONES_FOUND} -gt 0 ]]; then
-      SLAVE_FOUND="`cat ${THIS_INC} | egrep -c '^[[:space:]]*type[[:space:]]*slave[[:space:]]*;'`"
+      SLAVE_FOUND="$(cat <"${THIS_INC}" | grep -E -c '^[[:space:]]*type[[:space:]]*slave[[:space:]]*;')"
       if [[ ${SLAVE_FOUND} -eq 0 ]]; then
         RET_INC_FILES="$RET_INC_FILES $THIS_INC"
         echo "zone $ZONENAME found in $THIS_INC"
@@ -85,15 +157,13 @@ function reduce_include_files
   done
 }
 
-
 # check_conf <named.conf-path>
-function check_conf()
-{
+function check_conf() {
   NAMED_CONF=$1
   echo "Checking $NAMED_CONF named configuration file..."
   if [[ ! -r ${NAMED_CONF} ]]; then
     echo "File $NAMED_CONF is not readable: aborting..."
-    exit -1
+    exit 13 # ENOACCES
   fi
   must_have_include "$NAMED_CONF"
   add_any_include_files "$NAMED_CONF"
@@ -103,7 +173,7 @@ function check_conf()
   # Scan through 2nd-nested each include statement for "include"
   for THIS_INC_FILE in ${ALL_INC_FILES}; do
     # echo "Finally processing 2nd-nested $THIS_INCFILE..."
-    INC_FILES_FOUND="`cat ${THIS_INC_FILE} | egrep -c '^[[:space:]]*include '`"
+    INC_FILES_FOUND="$(cat <"${THIS_INC_FILE}" | grep -E -c '^[[:space:]]*include ')"
     if [[ ${INC_FILES_FOUND} -gt 0 ]]; then
       # echo "Include(s) found in $THIS_INCFILE"
       add_any_include_files "$THIS_INC_FILE"
@@ -115,7 +185,7 @@ function check_conf()
   # Scan through 3rd-nested each include statement for "include"
   for THIS_INC_FILE in ${SEC_INC_FILES}; do
     # echo "Finally processing 3rd-nested $THIS_INCFILE..."
-    INC_FILES_FOUND="`cat ${THIS_INC_FILE} | egrep -c '^[[:space:]]*include '`"
+    INC_FILES_FOUND="$(cat <"${THIS_INC_FILE}" | grep -E -c '^[[:space:]]*include ')"
     if [[ ${INC_FILES_FOUND} -gt 0 ]]; then
       # echo "Include(s) found in $THIS_INCFILE"
       add_any_include_files "$THIS_INC_FILE"
@@ -134,12 +204,12 @@ function check_conf()
 
     # and look for "zone" statement to submit to named-checkzone
     # extract zone name and if any its class as well
-    ZONE_STATEMENT="`cat ${THIS_ZONE_CONF} | egrep '^[[:space:]]*zone '`"
-    ZONENAME="`echo ${ZONE_STATEMENT} | awk -F ' ' '{print $2}' | sed -e 's/\"//g'`"
+    ZONE_STATEMENT="$(cat <"${THIS_ZONE_CONF}" | grep -E '^[[:space:]]*zone ')"
+    ZONENAME="$(echo "${ZONE_STATEMENT}" | awk -F ' ' '{print $2}' | sed -e 's/\"//g')"
     if [[ "$ZONENAME" == "." ]]; then
       continue
     fi
-    CLASS_NAME="`echo ${ZONE_STATEMENT} | awk -F ' ' '{print $3}' | sed -e 's/://'`"
+    CLASS_NAME="$(echo "${ZONE_STATEMENT}" | awk -F ' ' '{print $3}' | sed -e 's/://')"
     if [[ "$CLASS_NAME" == "{" ]]; then
       CLASS_NAME="IN"
     fi
@@ -148,14 +218,14 @@ function check_conf()
     fi
 
     # and look for "file" statement to submit to named-checkzone
-    FILE_FOUND="`cat ${THIS_ZONE_CONF} | egrep -c '^[[:space:]]*file '`"
+    FILE_FOUND="$(cat <"${THIS_ZONE_CONF}" | grep -E -c '^[[:space:]]*file ')"
     if [[ ${FILE_FOUND} -eq 0 ]]; then
       echo "No file statement found for Zone $ZONENAME "
       echo "    in file $THIS_ZONE_CONF"
       exit 1
     fi
-    FILESPEC="`cat ${THIS_ZONE_CONF} | egrep '^[[:space:]]*file ' | awk -F '"' '{ print $2}'`"
-    check_zone ${ZONENAME} ${CLASS_NAME} "$FILESPEC"
+    FILESPEC="$(cat <"${THIS_ZONE_CONF}" | grep -E '^[[:space:]]*file ' | awk -F '"' '{ print $2}')"
+    check_zone_data "${ZONENAME}" ${CLASS_NAME} "$FILESPEC"
   done
   # returns VIEWS_LIST
   named-checkconf -zj "$NAMED_CONF"
@@ -174,12 +244,14 @@ if [[ ${RET_STS} -ne 0 ]]; then
   echo "DHCP configuration failed sanity check"
   exit ${RET_STS}
 fi
-echo "Checking ISC dhcpd server lease file ..."
-dhcpd -d -q -f -t -T
-RET_STS=$?
-if [[ ${RET_STS} -ne 0 ]]; then
-  echo "DHCP lease failed sanity check"
-  exit ${RET_STS}
+if [ -f /var/lib/dhcpd/dhcpd.leases ]; then
+  echo "Checking ISC dhcpd server lease file ..."
+  dhcpd -d -q -f -t -T
+  RET_STS=$?
+  if [[ ${RET_STS} -ne 0 ]]; then
+    echo "DHCP lease failed sanity check"
+    exit ${RET_STS}
+  fi
 fi
 
 rndc "$INTERNAL_RNDC_OPT" -c "$INTERNAL_RNDC_CONF" reload
@@ -205,12 +277,9 @@ rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" sign "$PUBLIC_DOMAIN_FQDN" in red
 rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" sign "$PUBLIC_DOMAIN_FQDN"
 rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" notify "$PUBLIC_DOMAIN_FQDN"
 rndc "$INTERNAL_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" freeze "$INTERNAL_DOMAIN_FQDN" IN "$INTERNAL_VIEWED_DYNAMIC_ZONE"
-rm /var/lib/bind/*.jnl
-rm /var/cache/bind/*.jnl
+########################rm -f /var/lib/bind/*.jnl
+########################rm -f /var/cache/bind/*.jnl
 rndc "$INTERNAL_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" thaw "$INTERNAL_DOMAIN_FQDN" IN "$INTERNAL_VIEWED_DYNAMIC_ZONE"
-
-
-
 
 # Close DHCP server, Bind-Internal, then Bind-External
 echo "Stopping all DHCP client and DNS servers..."
@@ -218,10 +287,8 @@ systemctl stop dhclient@eth1.service
 systemctl stop bind9-internal.service
 systemctl stop bind9-public.service
 
-
 # Clean up any files
-rm -rf /var/lib/bind/internal/slave/*.slave
-
+rm -rf /var/lib/bind/internal/slave/*.slave # how were this *.slave got generated?
 
 # Start DHCP server, Bind-Internal, then Bind-External
 echo "Starting DHCP client"
@@ -238,30 +305,11 @@ check_conf "$PUBLIC_NAMED_CONF"
 check_conf "$INTERNAL_NAMED_CONF"
 
 # these series of 'rm' is needed until BIND 9.12+
-rm /var/cache/bind/*.jbk
-rm /var/cache/bind/internal/*.jbk
-rm /var/cache/bind/public/*.jbk
+bdr_bind_files_remove
+bdr_bind_files_remove_internal
+bdr_bind_files_remove_public
 
-rm /var/lib/bind/*.jnl
-rm /var/lib/bind/*.signed
-rm /var/lib/bind/internal/*.jnl
-rm /var/lib/bind/internal/*.signed
-rm /var/lib/bind/internal/master/*.jnl
-rm /var/lib/bind/internal/master/*.signed
-rm /var/lib/bind/internal/slave/*.jnl
-rm /var/lib/bind/internal/slave/*.signed
-rm /var/lib/bind/internal/dynamic/*.jnl
-rm /var/lib/bind/internal/dynamic/*.signed
-
-rm /var/lib/bind/public/*.jnl
-rm /var/lib/bind/public/*.signed
-rm /var/lib/bind/public/master/*.jnl
-rm /var/lib/bind/public/master/*.signed
-rm /var/lib/bind/public/slave/*.jnl
-rm /var/lib/bind/public/slave/*.signed
-rm /var/lib/bind/public/dynamic/*.jnl
-rm /var/lib/bind/public/dynamic/*.signed
-
+# Now power up internal nameserver
 systemctl start bind9-internal.service
 RET_STS=$?
 if [[ ${RET_STS} -ne 0 ]]; then
@@ -269,7 +317,7 @@ if [[ ${RET_STS} -ne 0 ]]; then
   exit ${RET_STS}
 fi
 
-
+# Now power up external nameserver
 systemctl start bind9-public.service
 RET_STS=$?
 if [[ ${RET_STS} -ne 0 ]]; then
@@ -277,13 +325,21 @@ if [[ ${RET_STS} -ne 0 ]]; then
   exit ${RET_STS}
 fi
 
-# Check status of DHCP server, Bind-Internal, then Bind-External
-systemctl status -l --lines=20 --no-pager isc-dhcp-server.service
-systemctl status -l --lines=20 --no-pager bind9-internal.service
-systemctl status -l --lines=20 --no-pager bind9-public.service
+check_systemctl_status isc-dhcp-server.service
+check_systemctl_status bind9-internal.service
+check_systemctl_status bind9-public.service
 
-rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" zonestatus "$PUBLIC_DOMAIN_FQDN"
+# It takes time to start these babies up
+sleep 2
+
+# Now get the slaves to re-request AXFR zone upload again...
+zone_slaves_retransfer
+
+rndc_zonestatus_internal
+rndc_zonestatus_public
 
 # This echo command below erases the RETSTS/$? from previous commands
 echo "End of $0"
 
+# Might want to put this message out as a 'wall' message
+# wall "$0: completed"
--- bind-reset.sh
+++ bind-reset.sh
@@ -12,20 +12,36 @@
 # Debian 8/9/10 file system layout
 # ISC Bind9 v9.15
 
+echo "Resetting ISC Bind server (no DHCP option)"
+echo
+
 PUBLIC_DOMAIN_FQDN="egbert.net"
 PUBLIC_BIND_DIR="/etc/bind"
 PUBLIC_NAMED_CONF="/etc/bind/named.conf"
 PUBLIC_RNDC_CONF="/etc/bind/rndc.conf"
 PUBLIC_RNDC_OPT="-p 953"
 
+# Get all the zones
+
+
 # check_zone <zone-name> <class> <zone-db-file>
 function check_zone()
 {
   echo "Performing named-checkzone -c $2 $1 $3..."
+  # named-checkzone -s relative -D -k fail   -m fail  -M fail -n fail -r fail -S fail -T warn -W fail -L 8399999 egbert.net /var/lib/bind/public/master/db.egbert.net
+
   named-checkzone \
+        -f raw \
+	-c $2 \
         -i full \
-        -S warn \
-        -c $2 \
+        -k fail \
+        -m fail \
+        -M fail \
+        -n fail \
+        -r fail \
+        -S fail \
+        -T warn \
+        -W fail \
         $1 $3
   RET_STS=$?
   if [[ ${RET_STS} -ne 0 ]]; then
@@ -56,22 +72,32 @@ function add_any_include_files()
   # echo "add_any_include_files: $INCLUDE_STATEMENTS"
 }
 
-# reduce_include_files <list>
-function reduce_include_files
+# reduce_include_files_to_having_slave_zones <list>
+function reduce_include_files_to_having_slave_zones
 {
+  NONSLAVE_ZONES=
   RET_INC_FILES=
   INC_FILES=$1
   for THIS_INC in ${INC_FILES}; do
+    # echo "Working on: $THIS_INC"
     # echo "Checking if $THISINC has any zone/view statements..."
     # find any zone or view
     ZONES_FOUND="`cat ${THIS_INC} | egrep -c '^[[:space:]]*zone '`"
+    # echo "ZONES_FOUND: $ZONES_FOUND"
     ZONE_STATEMENT="`cat ${THIS_INC} | egrep '^[[:space:]]*zone '`"
+    # echo "ZONE_STATEMENT: $ZONE_STATEMENT"
     ZONENAME="`echo ${ZONE_STATEMENT} | awk -F ' ' '{print $2}' | sed -e 's/\"//g'`"
+    # echo "ZONENAME: $ZONENAME"
     if [[ ${ZONES_FOUND} -gt 0 ]]; then
-      SLAVE_FOUND="`cat ${THIS_INC} | egrep -c '^[[:space:]]*type[[:space:]]*slave[[:space:]]*;'`"
-      if [[ ${SLAVE_FOUND} -eq 0 ]]; then
+      # Really should be an inverse pattern to 'master', not plain 'slave'
+      # But 'rndc' does not want 'slave', 'mirror', or 'stub'
+      SLAVE_FOUND="`cat ${THIS_INC} | egrep -c '^[[:space:]]*type[[:space:]]*(slave|mirror|stub)[[:space:]]*;'`"
+      if [[ ${SLAVE_FOUND} -eq 1 ]]; then
+        echo "  FOUND: Zone '$ZONENAME' is a slave"
         RET_INC_FILES="$RET_INC_FILES $THIS_INC"
-        echo "zone $ZONENAME found in $THIS_INC"
+        SLAVE_ZONENAMES="$ZONENAME $SLAVE_ZONENAMES"
+      else
+        echo "  IGNORED: Zone $ZONENAME is not a 'slave/mirror/stub' type."
       fi
     fi
   done
@@ -106,7 +132,7 @@ function check_conf()
 
   # Scan through 3rd-nested each include statement for "include"
   for THIS_INC_FILE in ${SEC_INC_FILES}; do
-    # echo "Finally processing 3rd-nested $THIS_INCFILE..."
+    # echo "Finally processing 3rd-nested $THIS_INC_FILE..."
     INC_FILES_FOUND="`cat ${THIS_INC_FILE} | egrep -c '^[[:space:]]*include '`"
     if [[ ${INC_FILES_FOUND} -gt 0 ]]; then
       # echo "Include(s) found in $THIS_INCFILE"
@@ -116,9 +142,9 @@ function check_conf()
   done
   # echo "All include files: $ALL_INCFILES"
 
-  reduce_include_files "$ALL_INC_FILES"
+  reduce_include_files_to_having_slave_zones "$ALL_INC_FILES"
   ALL_ZONE_CONFS="$RET_INC_FILES"
-  echo "Remaining include files having zone: $ALL_ZONE_CONFS"
+  # echo "Remaining include files having zone: $ALL_ZONE_CONFS"
 
   # We take list of named.conf files (or its include-derivative)
   # this assumes "file" statement is in same file as "zone" statement
@@ -158,6 +184,20 @@ function check_conf()
   fi
 }
 
+# Check named.conf syntax before doing anything harmful
+check_conf "$PUBLIC_NAMED_CONF"
+
+echo "ALL_ZONES_CONF: $ALL_ZONE_CONFS"
+for this_zone in $SLAVE_ZONENAMES; do
+  echo "rndc: refreshing '${this_zone}' zone ..."
+  rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" refresh ${this_zone}
+  RET_STS=$?
+  if [[ ${RET_STS} -ne 0 && ${RET_STS} -ne 1 ]]; then
+    echo "rndc failed to refresh zone '${ZONENAME}' server"
+    exit ${RET_STS}
+  fi
+done
+
 rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" reload
 RET_STS=$?
 if [[ ${RET_STS} -ne 0 && ${RET_STS} -ne 1 ]]; then
@@ -165,17 +205,42 @@ if [[ ${RET_STS} -ne 0 && ${RET_STS} -ne 1 ]]; then
   exit ${RET_STS}
 fi
 
+
 # Still a custom job to determine which zone (and view) to
 #  unfreeze for Dynamic DNS
 
 # Dynamic DNS is only used within Bind-Internal
 # Unfreeze Bind-Internal
+echo "Cleaning '$PUBLIC_DOMAIN_FQDN' zone ..."
 rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" sync -clean
+ret_sts=$?
+if [[ ${ret_sts} -ne 0 ]]; then
+  echo "rndc sync-clean FAILED! Error code $retsts; aborted."
+  exit $retsts
+fi
+echo "Signing on '$PUBLIC_DOMAIN_FQDN' zone in view 'red'..."
 rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" sign "$PUBLIC_DOMAIN_FQDN" in red
+ret_sts=$?
+if [[ ${ret_sts} -ne 0 ]]; then
+  echo "rndc sign $PUBLIC_DOMAIN_FQDN in red: Error code $retsts; aborted."
+  exit $retsts
+fi
+echo "Signing on '$PUBLIC_DOMAIN_FQDN' zone ..."
 rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" sign "$PUBLIC_DOMAIN_FQDN"
+ret_sts=$?
+if [[ ${ret_sts} -ne 0 ]]; then
+  echo "rndc sign $PUBLIC_DOMAIN_FQDN: Error code $retsts; aborted."
+  exit $retsts
+fi
+echo "Notifying master server for '$PUBLIC_DOMAIN_FQDN' zone ..."
 rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" notify "$PUBLIC_DOMAIN_FQDN"
-rm /var/lib/bind/*.jnl
-rm /var/cache/bind/*.jnl
+ret_sts=$?
+if [[ ${ret_sts} -ne 0 ]]; then
+  echo "rndc notify $PUBLIC_DOMAIN_FQDN: Error code $retsts; aborted."
+  exit $retsts
+fi
+rm -f /var/lib/bind/*.jnl
+rm -f /var/cache/bind/*.jnl
 
 
 # Close Bind
@@ -183,25 +248,25 @@ echo "Stopping all DNS servers..."
 systemctl stop bind9.service
 
 # Clean up any files
-rm -rf /var/lib/bind/slave/*.slave
+echo "Cleaning up files ..."
+rm -f /var/lib/bind/slave/*.slave
 
 # Start then Bind
 
-# Check namedconf
-check_conf "$PUBLIC_NAMED_CONF"
 
 # these series of 'rm' is needed until BIND 9.12+
-rm /var/cache/bind/*.jbk
+rm -f /var/cache/bind/*.jbk
 
-rm /var/lib/bind/*.jnl
-rm /var/lib/bind/*.signed
-rm /var/lib/bind/master/*.jnl
-rm /var/lib/bind/master/*.signed
-rm /var/lib/bind/slave/*.jnl
-rm /var/lib/bind/slave/*.signed
-rm /var/lib/bind/dynamic/*.jnl
-rm /var/lib/bind/dynamic/*.signed
+rm -f /var/lib/bind/*.jnl
+rm -f /var/lib/bind/*.signed
+rm -f /var/lib/bind/master/*.jnl
+rm -f /var/lib/bind/master/*.signed
+rm -f /var/lib/bind/slave/*.jnl
+rm -f /var/lib/bind/slave/*.signed
+rm -f /var/lib/bind/dynamic/*.jnl
+rm -f /var/lib/bind/dynamic/*.signed
 
+echo "Starting up bind9.service"
 systemctl start bind9.service
 RET_STS=$?
 if [[ ${RET_STS} -ne 0 ]]; then
@@ -210,11 +275,28 @@ if [[ ${RET_STS} -ne 0 ]]; then
 fi
 
 # Check status of then Bind
+echo "Checking on status of bind9.service"
 systemctl status -l --lines=20 --no-pager bind9.service
 
-rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" retransfer "$PUBLIC_DOMAIN_FQDN"
 sleep 2
+# Inform downstream (slaves and hidden master) nameservers of new zone update
+echo "rndc $PUBLIC_RNDC_OPT -c $PUBLIC_RNDC_CONF retransfer $PUBLIC_DOMAIN_FQDN"
+rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" retransfer "$PUBLIC_DOMAIN_FQDN"
+ret_sts=$?
+if [[ ${ret_sts} -ne 0 ]]; then
+  echo "rndc retransfer $PUBLIC_DOMAIN_FQDN: Error code $retsts; aborted."
+  exit $retsts
+fi
+
+# Probably can only do 'zonestatus' on master/primary, not slave/secondary here
+echo "rndc $PUBLIC_RNDC_OPT -c $PUBLIC_RNDC_CONF zonestatus $PUBLIC_DOMAIN_FQDN"
 rndc "$PUBLIC_RNDC_OPT" -c "$PUBLIC_RNDC_CONF" zonestatus "$PUBLIC_DOMAIN_FQDN"
+ret_sts=$?
+if [[ ${ret_sts} -ne 0 ]]; then
+  echo "rndc zonestatus $PUBLIC_DOMAIN_FQDN: Error code $retsts; aborted."
+  exit $retsts
+fi
+echo
 
 # This echo command below erases the RETSTS/$? from previous commands
 echo "End of $0"
